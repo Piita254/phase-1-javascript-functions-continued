d22a79b38de033ba7d53527eaa0c82dd591b035d
Peter Ndungu <petersonndungu995@gmail.com>
2024-04-01 13:16:54 +0300

    “completed”

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity) {
+    // If no activity is provided, default to "roller-skate"
+    if (!activity) {
+        activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let activity = "roller-skate";
+console.log(saturdayFun(activity)); 
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork());
+function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+}
+
+
+const encouragingPromptFunction = wrapAdjective("!!!"); 
+console.log(encouragingPromptFunction("amazing")); 
+console.log(encouragingPromptFunction()); 
